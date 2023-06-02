6c8495c77d023c8737368125581e07deee907a1e
nothexl <tishenko200477@gmail.com>
2023-06-03 03:10:11 +0400

    Первый коммит

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wild_animals
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        int imageId = 0;
+
+        private void changeImage(int imageId)
+        {
+            if (imageId == 0)
+            {
+                imageElement.Source = new BitmapImage(new Uri("..\\Pictures\\elephant.jpg", UriKind.Relative));
+                imageDesc.Text = "Слоно́вые, или слоны́, — семейство класса млекопитающих из отряда хоботных. В настоящее время к этому семейству относятся 3 ныне живущих вида. Африканские саванные слоны — наиболее крупные наземные млекопитающие.";
+                imageElement.Height = 450;
+            }
+            else if (imageId == 1)
+            {
+                imageElement.Source = new BitmapImage(new Uri("..\\Pictures\\giraffe.jpg", UriKind.Relative));
+                imageDesc.Text = "Жира́ф — парнокопытное млекопитающее из семейства жирафовых. Является самым высоким наземным животным планеты.";
+                imageElement.Height = 500;
+            }
+            else
+            {
+                imageElement.Source = new BitmapImage(new Uri("..\\Pictures\\tiger.jpg", UriKind.Relative));
+                imageDesc.Text = "Тигр — хищное млекопитающее семейства кошачьих, один из пяти видов рода пантер, принадлежащего к подсемейству больших кошек. Слово «тигр» происходит от др.-греч. τίγρις, которое в свою очередь восходит к др.-перс. *tigri от корня «*taig» со значением «острый; быстрый».";
+                imageElement.Height = 350;
+            }
+        }
+
+        private void leftArrow_Click(object sender, RoutedEventArgs e)
+        {
+            if (imageId == 0)
+                imageId = 2;
+            else
+                imageId--;
+
+            changeImage(imageId);
+        }
+
+        private void rightArrow_Click(object sender, RoutedEventArgs e)
+        {
+            if (imageId == 2)
+                imageId = 0;
+            else
+                imageId++;
+
+            changeImage(imageId);
+        }
+    }
+}
